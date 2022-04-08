4213a429e7deb3e15bd36253ff5b15f455a035fc
Antonia Castro <mariantonita@gmail.com>
2022-04-08 13:34:55 -0400

    restaurant site with js

--- Homework/week1/day3/index.js
+++ Homework/week1/day3/index.js
@@ -3,21 +3,24 @@ const dishName =[
     {name:"Chicken", price: 6.00},
     {name:"Beef", price: 7.00},
     {name:"Vegetables", price: 8.00},
-    {name:"Salad", price:5.00}
-    {name:"Soup", price];
+    {name:"Salad", price:5.00},
+    {name:"Soup", price: 4.00}
+];
 
 //----Arrays
 
 let dish = {
-    price: 1.00,
     orderSize: "size"
 }
 //--Hashes
 
-let cart = [];
 
+let cart = [];
+let balance = 0.00;
 for ( let i in dishName) {
-    cart += dishName[i]
-    cart += dish.price
+    cart += dishName[i].name
+    balance += dishName[i].price
     cart += dish.orderSize
 }
+
+console.log(cart)
