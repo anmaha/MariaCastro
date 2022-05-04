a7f38cc51ce73cdc45e9677f17e15f5fc385659a
Antonia Castro <mariantonita@gmail.com>
2022-05-03 21:11:10 -0400

    Space Battle New battle attack button and function

--- ClassCodeAlong/SpaceBattleOOPLab/app.js
+++ ClassCodeAlong/SpaceBattleOOPLab/app.js
@@ -1,31 +1,83 @@
-// get the elements
-let ship = document.querySelector('.ship');
-let alien =document.querySelectorAll('.alien');
+// get the DOM elements
+let ship = document.querySelector('.ship'); 
+console.log(ship);
+let aliens = [...document.querySelectorAll('.alien')]; //turns NodeList of images returned into an Array
+
 
 // attack functions
 const shipAttack = () =>{
-    console.log(" in shipAttack() ")
+    //console.log(" in shipAttack() ")
     ship.classList.toggle('ship-attack');
     console.log(ship)
 }
 
 const alienAttack = () =>{
-    alien[0].classList.toggle('alien-attack');
+    aliens[0].classList.toggle('alien-attack');
     console.log(alien)
 }
 
-if (Math.random() < alien[0].accuracy) {
+if (Math.random() < aliens[0].accuracy) {
 	console.log('You have been hit!');
 }
-
+// classes to make new ships
 class Ship {
-    constructor(hp,firepower,myMax,myMin){
-        this.hp = hp;
-        this.firepower = firepower;
-        this.accuracy = Math.random() * (myMax - myMin) + myMin;
+    constructor(){
+        this.hull = 20;
+        this.firepower = 5;
+        this.accuracy = 0.7;
+    }
+}
+
+class AlienShip { //aliens have hull(3,6) fp(2,4), accuracy(0.6,0.8)
+    constructor(){
+        this.hull = Math.random() * (6-3) + 3;
+        this.firepower = Math.random() * (4-2) + 2;
+        this.accuracy = Math.random() * (0.8 - 0.6) + 0.6;
+    }
+}
+
+let alien = []; // array to store new AlienShips with the div containing the alien
+aliens.forEach((element) => {
+    alien.push(Object.assign(new AlienShip(),{alien:element}));//copying a new object i.e. new AlienShip and adding a key:value of picture: string img source
+})
+console.log(alien);
+
+let ussAssembly = Object.assign(new Ship(),{ship:ship});
+
+console.log(ussAssembly);
+
+//DOM Elements
+const beginBattleButton = document.querySelector('.beginBattle');
+const USstats = document.querySelector('#ussScore');
+const alienStats = document.querySelector('#alienScore');
+
+//Begin Space Battle
+beginBattleButton.addEventListener('click', function(){
+    // Display the ussAssembly stats and alien[0] stats
+    console.log('Battle Begins');
+    alienStats.innerHTML = `Alien Ship Life is ${alien[0].hull}`;
+    USstats.innerHTML = `USS Assembly Life is ${ussAssembly.hull}`;
+    //ussAssembly attacks first alien ship alien[]
+    shipAttack();
+    if (Math.random() < aliens[0].accuracy) {
+        console.log('USS Assembly ');
+    }   else{
+        console.log('Alien Ship Hit!')
     }
 
-}
+
+})
+
+    //If the alien[0] survives, it attacks you
+//If you survive, you attack the ship again
+//If it survives, it attacks you again ... etc
+//If you destroy the ship, you have the option to attack the next ship or to retreat
+//If you retreat, the game is over, perhaps leaving the game open for further developments or options
+//You win the game if you destroy all of the aliens
+//You lose the game if you are destroyed
+
+
+
 
 // let command = prompt('Ready to [A]ttack or [R]etreat?')
 //     if(command === "A"){
